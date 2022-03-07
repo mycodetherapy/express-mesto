485092c073248f34b79495ab54d951aea28485b8
Max Ovchinnikov <ovchinnicov132@gmail.com>
2022-03-07 11:24:17 +0300

    feat: add logger

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const { PORT = 3000 } = process.env;
 const app = express();
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
 const { validateRegisterBody } = require('./validation');
 const { login, createUser } = require('./controllers/users');
@@ -21,13 +22,16 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   // useFindAndModify: false
 });
 
+app.use(requestLogger);
+
 app.post('/signin', validateRegisterBody, login);
 app.post('/signup', validateRegisterBody, createUser);
 app.use(auth);
 
 app.use('/users', require('./routes/users'));
-
 app.use('/cards', require('./routes/cards'));
+
+app.use(errorLogger);
 
 app.use((req, res, next) => {
   next(new NotFoundError('Маршрут не существует.'));
